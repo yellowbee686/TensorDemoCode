b68f556ebc5ed2bb10a8706fe7633d6fd17b7c40
yellowbee686 <yellowbee686@gmail.com>
2018-02-11 20:25:34 +0800

    commit openai_gym cartpole test code

---
+++ gym_test/cartpole/cartpole.py
@@ -0,0 +1,45 @@
+import numpy as np
+import json
+
+from tensorforce.agents import Agent
+from tensorforce.execution import Runner
+from tensorforce.contrib.openai_gym import OpenAIGym
+
+# Create an OpenAIgym environment
+env = OpenAIGym('CartPole-v0', visualize=False)
+
+network_path = 'ppo/ppo_network.json'
+agent_path = 'ppo/ppo.json'
+with open(network_path, 'r') as fp:
+    network_spec = json.load(fp=fp)
+with open(agent_path, 'r') as fp:
+    agent_config = json.load(fp=fp)
+agent = Agent.from_spec(
+    spec=agent_config,
+    kwargs=dict(
+        states_spec=env.states,
+        actions_spec=env.actions,
+        network_spec=network_spec
+    )
+)
+
+# Create the runner
+runner = Runner(agent=agent, environment=env)
+
+
+# Callback function printing episode statistics
+def episode_finished(r):
+    print("Finished episode {ep} after {ts} timesteps (reward: {reward})".format(ep=r.episode, ts=r.episode_timestep,
+                                                                                 reward=r.episode_rewards[-1]))
+    return True
+
+
+# Start learning
+runner.run(episodes=10000, max_episode_timesteps=200, episode_finished=episode_finished)
+runner.close()
+
+# Print statistics
+print("Learning finished. Total episodes: {ep}. Average reward of last 100 episodes: {ar}.".format(
+    ep=runner.episode,
+    ar=np.mean(runner.episode_rewards[-100:]))
+)
--- other_test/test_op.py
+++ other_test/test_op.py
@@ -1,5 +1,25 @@
 import tensorflow as tf
 
-x = [[[0., 0., 0.], [0., 0., 0.]], [[0., 0., 0.], [0., 0., 0.]]]
-sess = tf.Session()
-print(sess.run(tf.reduce_logsumexp(input_tensor=x, axis=-1)))
+# x = [[[0., 0., 0.], [0., 0., 0.]], [[0., 0., 0.], [0., 0., 0.]]]
+# sess = tf.Session()
+# print(sess.run(tf.reduce_logsumexp(input_tensor=x, axis=-1)))
+
+# a = tf.constant([2, 3], name='a')
+# b = tf.constant([[0, 1], [2, 3], [4, 5]], name='b')
+# x = tf.multiply(a, b, name='mul')
+# with tf.Session() as sess:
+#     print(sess.run(x))
+#
+# a = tf.constant([2, 2], name='a')
+# b = tf.constant([[0, 1], [2, 3]], name='b')
+# with tf.Session() as sess:
+# 	print(sess.run(tf.div(b, a)))             # [[0 0] [1 1]]
+# 	print(sess.run(tf.divide(b, a)))          # [[0. 0.5] [1. 1.5]]
+# 	print(sess.run(tf.truediv(b, a)))         # [[0. 0.5] [1. 1.5]]
+# 	print(sess.run(tf.floordiv(b, a)))        # [[0 0] [1 1]]
+# 	# print(sess.run(tf.realdiv(b, a)))         # # Error: only works for real values
+# 	print(sess.run(tf.truncatediv(b, a)))     # [[0 0] [1 1]]
+# 	print(sess.run(tf.floor_div(b, a)))       # [[0 0] [1 1]]
+
+W = tf.Variable(2)
+U = tf.Variable(2 * W.initialized_value())
